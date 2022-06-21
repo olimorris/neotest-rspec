877a6d79a001b9c1c11139f4b9bab2b611fcad80
Oli Morris <olimorris@users.noreply.github.com>
2022-06-21 14:40:40 +0100

    refactor: clean up utils

--- lua/neotest-rspec/utils.lua
+++ lua/neotest-rspec/utils.lua
@@ -1,32 +1,12 @@
 local logger = require("neotest.logging")
 
 local M = {}
---
+
 ---@param position neotest.Position The position to return an ID for
 ---@param namespaces neotest.Position[] Any namespaces the position is within
+---@return string
 M.generate_treesitter_id = function(position, namespace)
   return table.concat(vim.tbl_flatten({ position.path, position.range[1] }), "::")
-end
-
----@param position_id string
----@return string
-M.form_treesitter_id = function(position_id)
-  return position_id
-    :gsub("<Namespace>type:.-</Namespace> ", "") -- Remove any 'type: xx ' strings
-    :gsub(' <Namespace>"#', "#") -- Weird edge case
-    :gsub("<Test>should be_empty</Test>", "is expected to be empty") -- RSpec's one-liner syntax
-    :gsub("<Test>is_expected.to be_empty</Test>", "is expected to be empty") -- RSpec's one-liner syntax
-    :gsub("<Namespace>'", "")
-    :gsub("'</Namespace>", "")
-    :gsub('"</Namespace>', "")
-    :gsub('<Namespace>"', "")
-    :gsub("<Test>'", "")
-    :gsub("'</Test>", "")
-    :gsub('<Test>"', "")
-    :gsub('"</Test>', "")
-    :gsub("<Namespace>", "")
-    :gsub("<Namespace>", "")
-    :gsub("</Namespace>", "")
 end
 
 ---@param parsed_rspec_json table
