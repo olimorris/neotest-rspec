534640b6ad92195ed88b1fc2c2869c6bd408935b
AlexIv <alx.ekb@gmail.com>
2022-06-23 04:36:13 +0500

    Add more specs
    There is an issue running `:lua require('neotest').run.run()` from line spec/nested/basic_spec.rb:17
    test result doesn't match expected result
    
    running `:lua require('neotest').run.run(vim.fn.expand('%'))` from spec/class_method_spec.rb won't get expected result

--- spec/class_method_spec.rb
+++ spec/class_method_spec.rb
@@ -1,5 +1,12 @@
-RSpec.describe Class do
+require 'spec_helper'
+
+describe Class do
   describe '.new' do
+    # should be marked as skipped
+    xit 'skipped handles the "." method naming convention' do
+      expect(Class.new).to be_a(Class)
+    end
+
     it 'handles the "." method naming convention' do
       expect(Class.new).to be_a(Class)
     end
