25e5c53582f68fd011d3050e28c971cbd895f883
Richard Marbach <rmarbach@gmail.com>
2023-03-15 08:51:17 +0100

    feat: Support descriptionless it blocks

--- spec/one_liner_syntax_spec.rb
+++ spec/one_liner_syntax_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Array do
   describe 'when first created' do
     it { is_expected.to be_empty }
   end
+  describe 'when first created' do
+    it do
+      is_expected.to be_empty
+    end
+  end
 end
