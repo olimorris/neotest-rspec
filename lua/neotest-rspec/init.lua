97c05daedf49166d16da92281a5098820bc881a5
Josh Pencheon <josh.pencheon@googlemail.com>
2025-09-05 10:44:21 +0100

    revert change to engine regex from f5569be7d4625
    This broke support for engines, which are detected by the
    path to specs _not_ starting with 'spec/' (e.g. 'my_engine/spec/'
    instead).

--- lua/neotest-rspec/init.lua
+++ lua/neotest-rspec/init.lua
@@ -106,7 +106,7 @@ function NeotestAdapter.build_spec(args)
   local path = async.fn.expand("%")
 
   -- if the path starts with spec, it's a normal test. Otherwise, it's an engine test
-  local match = vim.regex("^spec/"):match_str(path)
+  local match = vim.regex("spec/"):match_str(path)
   if match and match ~= 0 then engine_name = string.sub(path, 0, match - 1) end
   local results_path = config.results_path()
 
